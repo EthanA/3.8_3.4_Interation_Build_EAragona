2fb85e00e96840991f9d4907ee520e9da85daceb
EthanA <86210158+EthanA@users.noreply.github.com>
2021-07-26 12:04:45 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,85 @@
+<!DOCTYPE html>
 
+<html lang="en">
+
+	<head>
+	
+		<title>Home</title>
+	
+		<meta charset="utf-8">
+		
+		<meta name="Keywords" content="html5, layout, CSS Grid System" />
+		
+		<meta name="Author" content="Ethan Aragona" />
+		
+		<meta name="Description" content="CSS grid system practise" />
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="stylesheet" href="css.css">
+	
+	</head>
+
+	<body>
+	
+		<div class="home-grid-container">
+			
+			<div class="nav">
+			
+				<ul>
+				
+					<li><a href="index.html">Home<a></li>
+					
+					<li><a>Tours</a>
+					
+						<ul>
+						
+							<li><a href="gannets.html">Gannets</a></li>
+							
+							<li><a>Geology</a></li>
+						
+						</ul>
+					
+					</li>
+					
+					<li><a>Contact</a></li>
+				
+				</ul>
+			
+			</div>
+			
+			<div class="header">Header</div>
+			
+			<div></div>
+				
+			<div class="home-content">
+			
+				<div class="scroll-area">
+				<h1>Lorem Ipsum</h1>
+				Lorem ipsum dolor sit amet, 
+				consectetur adipiscing elit. Aliquam tempor leo condimentum 
+				diam lacinia congue. Donec venenatis, eros ac elementum congue, 
+				sem mi porttitor magna, at cursus sem mauris ac felis. Aliquam 
+				erat volutpat. Donec justo arcu, posuere pharetra commodo sagittis
+				, placerat in turpis. Morbi ultrices pharetra ligula. Integer et 
+				elementum lorem, a imperdiet mauris. Duis accumsan ex felis. Nam 
+				ac felis pretium augue suscipit suscipit a ac sapien. Ut vehicula 
+				augue quis facilisis molestie. In quis sapien consequat, porttitor
+				lorem a, lacinia arcu. Suspendisse potenti. Vestibulum nec 
+				tristique orci. Phasellus ligula velit, pellentesque dapibus 
+				lacus ac, lobortis tristique ligula. Donec tempus sem non quam 
+				mollis aliquam. Donec condimentum metus nec lectus scelerisque 
+				ornare. Phasellus sit amet fringilla augue, non feugiat nunc.
+				</div>
+				
+			</div>
+				
+			<div></div>
+			
+			<div class="footer">Footer</div>
+		
+		</div>
+	
+	</body>
+	
+</html>
